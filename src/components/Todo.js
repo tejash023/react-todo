3eff8229750c4ab354295fae26c164e7f03b4e33
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-02-25 19:02:36 +0530

    fetch todos from api

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,13 @@
 import styles from "../css/todo.module.css";
-import TodoInput from "./TodoInput";
+
+import TodoList from "./TodoList";
 
 function Todo() {
   return (
     <div className={styles.container}>
       <h3>Todo List</h3>
-      <TodoInput />
+
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,33 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styles from "../css/todo.module.css";
 
 function TodoList() {
-  return <div>TodoList</div>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      console.log(response.data);
+      setTodos(response.data);
+      console.log("todos", todos);
+    };
+
+    fetchTodos();
+  }, [todos]);
+  return (
+    <>
+      <div className={styles.todoinput}>
+        <input />
+        <button>Add</button>
+      </div>
+      {todos.slice(0, 5).map((todo) => (
+        <p key={todo.id}>{todo.title}</p>
+      ))}
+    </>
+  );
 }
 
 export default TodoList;
